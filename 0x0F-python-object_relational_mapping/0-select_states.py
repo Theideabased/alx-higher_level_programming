41b6e27b9f7d20321c77338df9133c8c7e04b8bd
Theideabased <ogunmusireseyi01@gmail.com>
2023-03-20 21:33:11 -0700

    sql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,24 @@
-from sqlalchemy.engine.url import URL
+#!/usr/bin/python3
+"""
+This script lists all states from the 
+database 'hbtn_0e_0_usa'.
+"""
 
-postgres_db = {'drivername': 'postgres',
-               'username': 'postgres',
-               'password': 'postgres',
-               'host': '192.168.99.100',
-               'port': 5432}
-print(URL(**postgres_db))
 
-sqlite_db = {'drivername': 'sqlite', 'database': 'db.sqlite'}
-print(URL(**sqlite_db))
+import MySQLdb
+from sys import argv
+
+if __name == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+            passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
