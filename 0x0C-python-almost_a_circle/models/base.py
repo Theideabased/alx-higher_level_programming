61a3f97ca48d09c64a3b1587e2fea9ddafc31bab
Theideabased <ogunmusireseyi01@gmail.com>
2023-07-16 07:10:26 -0700

    done

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" class to get index """
+
+
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        self(int)
+        id(int)
+        """
+        if id not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base__nb_objects
+            
