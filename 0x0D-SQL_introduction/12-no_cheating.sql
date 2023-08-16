e787b576c40e617815118310dab21f0322c6acb7
Theideabased <ogunmusireseyi01@gmail.com>
2023-08-15 18:19:58 -0700

    more sql

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- This will change bob score to 10
+USE hbtn_0c_0
+UPDATE second_table
+SET 
+score = 10
+WHERE name = 'Bob';
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- this will remove all the row with score less tgan 5
+USE hbtn_0c_0
+DELETE FROM second_table
+WHERE score <= 5;
