719e640bb1972d2001d64fba84e29fa47ee6ffea
Theideabased <ogunmusireseyi01@gmail.com>
2023-08-16 20:15:48 -0700

    granting privilegdes

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- This will create a database and a user
+-- The user can only have the SELECT permission on the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 
+'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+ON hbtn_0d_2
+TO 'user_0d_2'@'localhost';
