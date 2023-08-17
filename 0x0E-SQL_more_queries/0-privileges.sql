645c590a47a10367badada7e8cba9b9de3222a97
Theideabased <ogunmusireseyi01@gmail.com>
2023-08-16 19:50:21 -0700

    show priviledges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- This will list all the priviledges on the database
-SHOW GRANTS FOR 'user_0d_'1@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_'1@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
