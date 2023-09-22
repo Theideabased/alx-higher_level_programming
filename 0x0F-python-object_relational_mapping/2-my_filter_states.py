5d2bcc6c1eab438c7b69eda696f4165e0dc20c2e
Theideabased <ogunmusireseyi01@gmail.com>
2023-09-21 17:39:27 -0700

    filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,22 +2,23 @@
 """  this will take and argument and print out
 the table that resemble the argument"""
 
-# import the library
-import MySQLdb
-import sys
+if __name__ == '__main__':
+    # import the library
+    import MySQLdb
+    from sys import argv
 
-# connecting to mysql server
-conn = MySQLdb.connect(host="localhost", user="root", passwd="root",\
-        db="hbtn_0e_0_usa", charset="utf8", port=3306)
-cur = conn.cursor()
+    # connecting to mysql server
+    conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                           db=argv[3], charset="utf8", port=3306)
+    cur = conn.cursor()
 
-# executing my sql code
-cur.execute("SELECT * FROM states WHERE name = %s", (sys.argv,))
-query_row = cur.fetchall()
-for row in query_row:
-    print(row)
+    # executing my sql code
+    cur.execute(f"SELECT * FROM states WHERE name LIKE BINARY '{argv[4]}'\
+            ORDER BY states.id ASC")
+    query_row = cur.fetchall()
+    for row in query_row:
+        print(row)
 
-# closing the connection with my server
-cur.close()
-conn.close()
-
+    # closing the connection with my server
+    cur.close()
+    conn.close()
