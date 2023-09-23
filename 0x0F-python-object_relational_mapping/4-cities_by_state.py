7141f19fb047c93e71d856b7117f4f6b80846f90
Theideabased <ogunmusireseyi01@gmail.com>
2023-09-22 19:03:46 -0700

    cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,22 +2,25 @@
 """ this code will print all in cities from the hbtn_0e_4_usa
 database"""
 
-# importing the important library
-import MySQLdb
+if __name__ == '__main__':
 
-# connecting to mysql server
-conn = MySQLdb.connect(port=3306, host='localhost', passwd='root', user='root',\
-        db='hbtn_0e_4_usa', charset='utf8')
-cur = conn.cursor()
+    # importing the important library
+    import MySQLdb
+    from sys import argv
 
-# executing the SQL codes
-cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    # connecting to mysql server
+    conn = MySQLdb.connect(port=3306, host='localhost', passwd=argv[2],
+                           user=argv[1], db=argv[3], charset='utf8')
+    cur = conn.cursor()
+
+    # executing the SQL codes
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
         LEFT OUTER JOIN states ON cities.state_id=states.id \
         ORDER BY cities.id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
 
-# closing my database connections
-cur.close()
-conn.close()
+    # closing my database connections
+    cur.close()
+    conn.close()
