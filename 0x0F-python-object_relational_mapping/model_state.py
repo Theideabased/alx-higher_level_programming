541d37c17de6cd89fe1c286f3a3c4cf255d46a06
Theideabased <ogunmusireseyi01@gmail.com>
2023-09-22 20:25:37 -0700

    first stat model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,25 +3,16 @@
 database using the ORM format """
 
 # importing need library
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy import Column, String, Integer
+
 Base = declarative_base()
 
-# connecting to mysql database
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root", \
-        "root", "hbtn_0e_6_usa"), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
 
 # writing my database class
 class State(Base):
     """ This class will create my database
     with my linked mysql database """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
-    name = Column('name', String(128))
-    
-session.close()
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column('name', String(128), nullable=False)
