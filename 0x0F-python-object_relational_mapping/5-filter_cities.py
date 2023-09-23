c49935e82b511f0f284071dc8e5f39d682b9ed84
Theideabased <ogunmusireseyi01@gmail.com>
2023-09-22 19:47:03 -0700

    all cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,28 +2,23 @@
 """ this code will print all in cities from the hbtn_0e_4_usa
 database"""
 
-# importing the important library
-import MySQLdb
-import sys
+if __name__ == '__main__':
+    # importing the important library
+    import MySQLdb
+    from sys import argv
 
-# making sure that only one argument is given
-if len(sys.argv) > 2:
-    print("Usage: python3 filename.py Argument")
-    sys.exit(1)
+    # connecting to mysql server
+    conn = MySQLdb.connect(port=3306, host='localhost', passwd=argv[2],
+                           user=argv[1], db=argv[3], charset='utf8')
+    cur = conn.cursor()
 
-# connecting to mysql server
-conn = MySQLdb.connect(port=3306, host='localhost', passwd='root', user='root',\
-        db='hbtn_0e_4_usa', charset='utf8')
-cur = conn.cursor()
+    # executing the SQL codes
+    cur.execute("SELECT cities.name FROM cities LEFT OUTER JOIN states \
+       ON cities.state_id=states.id WHERE states.name=%s", (argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
 
-# executing the SQL codes
-cur.execute("SELECT cities.name FROM cities LEFT OUTER JOIN states \
-       ON cities.state_id=states.id WHERE states.name=%s",\
-        (sys.argv[1],))
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-
-# closing my database connections
-cur.close()
-conn.close()
+    # closing my database connections
+    cur.close()
+    conn.close()
