094fba36cf7b6dc965fc72379646e5511681040e
Theideabased <ogunmusireseyi01@gmail.com>
2023-09-22 20:57:37 -0700

    first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,13 +2,15 @@
 """ listing all the state from the hbtn_0e_6_usa """
 
 # importing the needed library
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # connecting to mysql database
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root",
-    "hbtn_0e_6_usa"), pool_pre_ping=True)
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                       .format(argv[1], argv[3], argv[3]),
+                       pool_pre_ping=True)
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
