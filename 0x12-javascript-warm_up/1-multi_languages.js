38ff708be54a0fb215756febdc976b1d03a58827
Theideabased <ogunmusireseyi01@gmail.com>
2023-09-27 20:46:21 -0700

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// print out using console.log
+
+// define the variables
+const myVar = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+
+// print the variables in the terminal
+console.log(myVar);
+console.log(myVar2);
+console.log(myVar3);
